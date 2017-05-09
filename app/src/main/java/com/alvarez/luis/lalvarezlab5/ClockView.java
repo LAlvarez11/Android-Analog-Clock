7b856852e910ad9006231e4b7094555e659a42fd
luisalvarez <alvarezluis121@gmail.com>
2017-05-09 10:09:44 -0700

    ClockView Update

--- app/src/main/java/com/alvarez/luis/lalvarezlab5/ClockView.java
+++ app/src/main/java/com/alvarez/luis/lalvarezlab5/ClockView.java
@@ -1,5 +1,7 @@
 package com.alvarez.luis.lalvarezlab5;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
@@ -7,5 +9,37 @@ import android.view.View;
  */
 
 public class ClockView extends View {
+
+    private Context context;
+
+    public ClockView(Context context) {
+        super(context);
+        if(!isInEditMode()){
+            init(context);
+        }
+    }
+
+
+
+    public ClockView(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+        if(!isInEditMode()){
+            init(context);
+        }
+    }
+
+    public ClockView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if(!isInEditMode()){
+            init(context);
+        }
+
+    }
+
+    private void init(Context context){
+        this.context = context;
+    }
+
 
 }
